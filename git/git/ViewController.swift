e32cd42940c12907b0c2bfc1afc8168c293aff8e
pavaniKavulu <pavanikavulu@gmail.com>
2021-02-17 10:11:10 +0530

    Made changes.. Just have a look!!

---
+++ git/git/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  git
+//
+//  Created by PAVANI on 17/02/21.
+//  Copyright © 2021 PAVANI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Simple Rep")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
