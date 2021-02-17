17b3acaeeb686d3a60b00381d946c74388282500
pavaniKavulu <pavanikavulu@gmail.com>
2021-02-17 11:51:19 +0530

    Created a struct File

---
+++ git/git/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  git
+//
+//  Created by PAVANI on 17/02/21.
+//  Copyright © 2021 PAVANI. All rights reserved.
+//
+
+import Foundation
+
+struct File {
+    
+    var name:String!
+    
+    func delegate(){
+        
+    }
+}
